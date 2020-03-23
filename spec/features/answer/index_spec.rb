a40ba83007831a3a7040cb39566953416e895ab1
vtka <slavik.tkachenko@gmail.com>
2020-03-23 18:17:44 +0600

    add index_spec for Answer

--- spec/features/answer/index_spec.rb
+++ spec/features/answer/index_spec.rb
@@ -1,20 +1,19 @@
 require 'rails_helper'
 
-feature 'User can see question list in order to find answers' do
-
+feature 'User can see question\s answer list list in order to find suitable answers' do
   given(:user) { create(:user) }
-  given!(:questions) { create_list(:question, 3, author: user) }
+  given!(:question) { create(:question, author: user) }
+  given!(:answers) { create_list(:answer, 3, author: user, question: question) }
 
-  scenario 'Authenticated user sees list of all questions' do
+  scenario 'Authenticated user sees list of all answers' do
     sign_in(user)
-    visit questions_path
+    visit question_path(question)
 
-    questions.each { |question| expect(page).to have_content question.title }
+    answers.each { |answer| expect(page).to have_content answer.body }
   end
 
-  scenario 'Unauthenticated user sees list of all questions' do
-    visit questions_path
-    questions.each { |question| expect(page).to have_content question.title }
+  scenario 'Unauthenticated user sees list of all answers' do
+    visit question_path(question)
+    answers.each { |answer| expect(page).to have_content answer.body }
   end
-
 end
