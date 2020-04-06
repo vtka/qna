015bffe8ab7a43636e36cacbb517cc76f17fb5be
vtka <slavik.tkachenko@gmail.com>
2020-04-06 20:59:33 +0600

    fix config for jquery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,7 @@
 const webpack = require('webpack')
-environment.plugins.prepend(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
-  })
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery'
+    })
 )
