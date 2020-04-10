973c801757a18e0dfa86b15de8699b2c600be31e
vtka <slavik.tkachenko@gmail.com>
2020-04-10 15:00:16 +0600

    complete edit_spec acceptance test

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -18,23 +18,44 @@ feature 'User can edit his answer', %q{
     end
   end
 
-  describe 'Authenticated user' do
-    scenario 'edits his answer', js: true do
-      sign_in(users.first)
+  describe 'Authenticated user', js: true do
+    describe do
+      background do
+        sign_in(users.first)
+  
+        visit question_path(question)
+      end
+      
+      scenario 'edits his answer' do
+        within '.answers' do
+          click_on 'Edit'
+          fill_in 'Your answer', with: 'Edited answer'
+          click_on 'Save'
+
+          expect(page).to_not have_content answer.body
+          expect(page).to have_content 'Edited answer'
+          expect(page).to_not have_selector 'textarea'
+        end
+      end
+
+      scenario 'edits answer with invalid params' do
+        within '.answers' do
+          click_on 'Edit'
+          fill_in 'Your answer', with: ''
+          click_on 'Save'
+
+          expect(page).to have_content answer.body
+          expect(page).to have_selector 'textarea'
+        end
+      end
+    end
+    scenario "tries to edit other user's answer" do
+      sign_in(users.last)
       visit question_path(question)
       
       within '.answers' do
-        click_on 'Edit'
-        fill_in 'Your answer', with: 'Edited answer'
-        click_on 'Save'
-
-        expect(page).to_not have_content answer.body
-        expect(page).to have_content 'Edited answer'
-        expect(page).to_not have_selector 'textarea'
+        expect(page).to_not have_link 'Edit'
       end
     end
-
-    scenario 'edits answer with invalid params'
-    scenario "tries to edit other user's answer"
   end
 end
