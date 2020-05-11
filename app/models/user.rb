ef91c5e63491352b1d74d7f73fb41dc7c6165195
vtka <slavik.tkachenko@gmail.com>
2020-05-11 23:36:57 +0600

    Create Authorizations; Add specs for user registration via third party;

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :badges, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :comments, foreign_key: 'author_id', dependent: :delete_all
+  has_many :authorizations
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
@@ -21,7 +22,24 @@ class User < ApplicationRecord
   end
 
   def self.find_for_oauth(auth)
-    
+    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    return authorization.user if authorization
+
+    email = auth.info[:email]
+    user = User.where(email: email).first
+
+    if user
+      user.create_authorization(auth)
+    else
+      password = Devise.friendly_token[0, 20]
+      user = User.create!(email: email, password: password, password_confirmation: password)
+      user.create_authorization(auth)
+    end
+
+    user
   end
 
+  def create_authorization(auth)
+    self.authorizations.create(provider: auth.provider, uid: auth.uid)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_113318) do
+ActiveRecord::Schema.define(version: 2020_05_11_163759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,15 @@ ActiveRecord::Schema.define(version: 2020_05_06_113318) do
     t.boolean "best", default: false, null: false
     t.index ["author_id"], name: "index_answers_on_author_id"
     t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "authorizations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "provider"
+    t.string "uid"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_authorizations_on_user_id"
   end
 
   create_table "badges", force: :cascade do |t|
@@ -113,6 +122,7 @@ ActiveRecord::Schema.define(version: 2020_05_06_113318) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "answers", "questions"
   add_foreign_key "answers", "users", column: "author_id"
+  add_foreign_key "authorizations", "users"
   add_foreign_key "badges", "questions"
   add_foreign_key "badges", "users"
   add_foreign_key "comments", "users", column: "author_id"
