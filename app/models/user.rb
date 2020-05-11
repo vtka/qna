ef3b21771d307bc0baff60b64e67b05ba2153de3
vtka <slavik.tkachenko@gmail.com>
2020-05-12 00:40:55 +0600

    Refactor God Object; Create Oauth Service; Refactor specs accordingly

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :badges, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :comments, foreign_key: 'author_id', dependent: :delete_all
-  has_many :authorizations
+  has_many :authorizations, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
@@ -22,21 +22,7 @@ class User < ApplicationRecord
   end
 
   def self.find_for_oauth(auth)
-    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
-    return authorization.user if authorization
-
-    email = auth.info[:email]
-    user = User.where(email: email).first
-
-    if user
-      user.create_authorization(auth)
-    else
-      password = Devise.friendly_token[0, 20]
-      user = User.create!(email: email, password: password, password_confirmation: password)
-      user.create_authorization(auth)
-    end
-
-    user
+    FindForOauthService.new(auth).call
   end
 
   def create_authorization(auth)
