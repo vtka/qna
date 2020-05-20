9cc2f319395d6e5f33405edf6ce521ef527e7831
vtka <slavik.tkachenko@gmail.com>
2020-05-20 16:26:29 +0600

    Add comment_serializer; Add files, comments to AnswersController API

--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -9,7 +9,7 @@ describe 'Answers API', type: :request do
   let(:access_token) { create :access_token }
   let(:user) { create :user }
   let(:question) { create :question, author: user }
-  let!(:answers) { create_list :answer, 3, author: user, question: question }
+  let!(:answers) { create_list :answer, 3, :with_file, author: user, question: question }
   let(:answer) { answers.first }
 
   describe 'GET /api/v1/questions/:question_id/answers' do
@@ -48,6 +48,10 @@ describe 'Answers API', type: :request do
 
     context 'authorized' do
       let(:answer_response) { json['answer'] }
+      let!(:comments) { create_list :comment, 3, author: user, commentable: answer }
+      let(:comment) { comments.first }
+      let(:comment_response) { answer_response['comments'].first }
+      let!(:links) { create_list :link, 4, linkable: answer }
 
       before { get api_path, params: { access_token: access_token.token }, headers: headers }
 
@@ -60,6 +64,26 @@ describe 'Answers API', type: :request do
           expect(answer_response[attr]).to eq answer.send(attr).as_json
         end
       end
+
+      context 'comments' do
+        it 'are returned all' do
+          expect(answer_response['comments'].size).to eq 3
+        end
+
+        it 'returns all public fields' do
+          %w[id body created_at updated_at author_id].each do |attr|
+            expect(comment_response[attr]).to eq comment.send(attr).as_json
+          end
+        end
+      end
+
+      it 'returns links' do
+        expect(answer_response['links'].size).to eq 4
+      end
+
+      it 'returns files' do
+        expect(answer_response['files'].first.size).to eq 2 
+      end
     end
   end
-end
+endg
