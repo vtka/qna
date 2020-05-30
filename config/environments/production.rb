0b39a54b4807884029f6235ff202465c92fd5832
vtka <slavik.tkachenko@gmail.com>
2020-05-30 21:21:45 +0600

    Add Mailer configs; Edit credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,19 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: '185.14.186.40' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings =
+    {
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: 'gmail.com',
+      authentication: :plain,
+      user_name: Rails.application.credentials[:production][:gmail][:username],
+      password: Rails.application.credentials[:production][:gmail][:password]
+    }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
